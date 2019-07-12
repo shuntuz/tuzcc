606267eed514e62498339113bbedce8b1a8cdb2e
Shunsuke Tsuzuki <tsuzukis@TUZBookPro13.local>
2019-07-12 22:28:11 +0900

    add addition and subtract

--- test.sh
+++ test.sh
@@ -3,7 +3,7 @@ try() {
   expected="$1"
   input="$2"
 
-  ./9cc "$input" > tmp.s
+  ./tuzcc "$input" > tmp.s
   gcc -o tmp tmp.s
   ./tmp
   actual="$?"
@@ -18,4 +18,5 @@ try() {
 
 try 0 0
 try 42 42
+try 21 '5+20-4'
 
